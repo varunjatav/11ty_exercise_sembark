b6588dbdde152b2f50e718a9d01c47ac3064bc17
Varun Jatav <mohit.mohit979@gmail.com>
2023-07-22 20:01:42 +0530

    Web design

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("video.mp4");
-    eleventyConfig.addPassthroughCopy("tour.mp4");
+    eleventyConfig.addPassthroughCopy("assets/video.mp4");
+    eleventyConfig.addPassthroughCopy("assets/tour.mp4");
+    eleventyConfig.addPassthroughCopy("assets/favicon.png");
 };
