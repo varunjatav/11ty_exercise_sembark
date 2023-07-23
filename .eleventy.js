9c8a31b4f6eb16dd0b04a5f7a86d6654a4f9b8a4
Varun Jatav <mohit.mohit979@gmail.com>
2023-07-23 12:08:05 +0530

    travels

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,19 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets/video.mp4");
     eleventyConfig.addPassthroughCopy("assets/tour.mp4");
     eleventyConfig.addPassthroughCopy("assets/favicon.png");
+    eleventyConfig.addPassthroughCopy("./main.css");
+    
+      eleventyConfig.addPassthroughCopy("src");
+    return { 
+        dir: { 
+        input: "src", 
+        includes:"../_includes",
+        output: "_site" ,
+        layout: "layouts/travel_root.njk"
+    },
+    templateFormats: ["html", "liquid", "njk"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    
+}; 
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,32 @@ module.exports = {
       fontFamily: {
         'sans': ['"Noto Sans"', ...defaultTheme.fontFamily.sans],
       },
+      backgroundImage:{
+        'hero-Img' : "url('https://www.odtravels.in/playground/themes/images/odisha-banner/odisha-puri-tour-banner.jpg')",
+      },
+      screens: {
+        'mobile': '480px',
+        // => @media (min-width: 480px) { ... }
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+      backgroundColor:{
+        gray: "#ededed",
+        green: "#00ff00",
+      },
+      
+      colors:{
+        cyan: "#00ffff",
+        blue: "#3b3bff",
+        pink: "#ff748c",
+        yellow: "#e6e600",
+      },
     },
   },
   plugins: [
