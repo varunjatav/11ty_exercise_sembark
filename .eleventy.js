317f4878f5483b72a0a21530858718862b76f4e1
Varun Jatav <mohit.mohit979@gmail.com>
2023-07-28 17:45:54 +0530

    completed

--- .eleventy.js
+++ .eleventy.js
@@ -3,5 +3,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets/tour.mp4");
     eleventyConfig.addPassthroughCopy("assets/favicon.png");
     eleventyConfig.addPassthroughCopy("./main.css");
+    eleventyConfig.addPassthroughCopy("img1.png");
    
 };
